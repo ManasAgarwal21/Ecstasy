df9b83708643bc8197a445f1736ffeadb9ef9ddf
rupin-1801 <rupinbhugra1411@gmail.com>
2021-08-26 17:13:20 +0530

    added signup component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Login from './components/login';
+import Signup from './components/signup';
 
 function App() {
   return (
-    <Login/>
+    <React.Fragment>
+      <Login/>
+      <Signup/>
+    </React.Fragment>
   );
 }
 
