cbf25544aa1b1b05a32775a525750624a8070a2d
rupin-1801 <rupinbhugra1411@gmail.com>
2021-08-26 13:45:14 +0530

    added login component and app router component added app-router in index.js

---
+++ src/components/login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const login = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default login;
+// git commit -m "added login component and app router component added app-router in app.js"
