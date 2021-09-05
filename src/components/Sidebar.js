d26c496a9d970e0e13e54f2407e0e814179cdffb
ManasAgarwal21 <manasagarwal162000@gmail.com>
2021-09-05 15:35:42 -0700

    changed sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,6 +8,12 @@ import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import InboxIcon from "@material-ui/icons/MoveToInbox";
 import MailIcon from "@material-ui/icons/Mail";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 const useStyles = makeStyles({
   list: {
@@ -15,6 +21,10 @@ const useStyles = makeStyles({
   },
   fullList: {
     width: "auto",
+  },
+  root: {
+    width: "100%",
+    maxWidth: 360,
   },
 });
 
@@ -24,8 +34,17 @@ const Sidebar = ({ isSidebarOpen, setIsSidebarOpen }) => {
 
   const list = () => (
     <div className={classes.list} role="presentation">
-      <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+      <List
+        component="nav"
+        aria-labelledby="Shop By Category"
+        subheader={
+          <ListSubheader component="div" id="shopByCategory" style={{position:"static"}}>
+            Shop By Category
+          </ListSubheader>
+        }
+        className={classes.root}
+      >
+        {["MEN", "WOMEN", "HEALTH", "MOBILES"].map((text, index) => (
           <ListItem button key={text}>
             <ListItemIcon>
               {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
@@ -33,10 +52,67 @@ const Sidebar = ({ isSidebarOpen, setIsSidebarOpen }) => {
             <ListItemText primary={text} />
           </ListItem>
         ))}
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="See All Categories"
+            id="seeAllCategories"
+          >
+            <Typography>See All</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <List style={{ marginLeft: "-18px", marginTop: "-30px" }}>
+              {["MEN", "WOMEN", "HEALTH", "MOBILES"].map((text, index) => (
+                <ListItem button key={text}>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItem>
+              ))}
+            </List>
+          </AccordionDetails>
+        </Accordion>
+      </List>
+      <List
+        component="nav"
+        aria-labelledby="Quick Links"
+        subheader={
+          <ListSubheader component="div" id="quickLinks">
+            Quick Links
+          </ListSubheader>
+        }
+        className={classes.root}
+      >
+        {["Your Orders", "Your Favourites", "Your Coupons", "Your Account"].map(
+          (text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          )
+        )}
       </List>
       <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
+      <List
+        component="nav"
+        aria-labelledby="Help and Settings"
+        subheader={
+          <ListSubheader component="div" id="helpAndSettings">
+            Help And Settings
+          </ListSubheader>
+        }
+        className={classes.root}
+      >
+        {[
+          "Choose Language",
+          "Add/Change Address",
+          "Customer Service",
+          "Privacy Policy",
+          "Logout",
+        ].map((text, index) => (
           <ListItem button key={text}>
             <ListItemIcon>
               {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
