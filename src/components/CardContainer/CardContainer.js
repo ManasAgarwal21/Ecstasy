6f9bc9181c26375aab1600afebf7fe0839b7e21b
rupin-1801 <rupinbhugra1411@gmail.com>
2021-09-11 11:58:30 +0530

    updated box shadow of card container

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -25,7 +25,7 @@ const useStyles = makeStyles((theme) => ({
   fullWidthContainer: {
     margin: "20px",
     padding: "0px 10px",
-    // boxShadow: "0px 1px 8px rgba(0,0,0,0.2)",
+    boxShadow: "0px 1px 8px rgba(0,0,0,0.2)",
   },
   head: {
     alignItems: "center",
