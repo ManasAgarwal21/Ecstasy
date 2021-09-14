aee20834c4066f21db6a366a92363605fb9badd0
rupin-1801 <rupinbhugra1411@gmail.com>
2021-09-15 01:28:34 +0530

    merged cardcontaner and imagelistcontainer in one component

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -2,10 +2,16 @@ import React from "react";
 import { Paper, Typography, Grid, IconButton } from "@material-ui/core";
 import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
 import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import { useSelector } from "react-redux";
+import { getProducts } from "../../redux/selectors/user.selectors";
+import ImageList from "@material-ui/core/ImageList";
+import ImageListItem from "@material-ui/core/ImageListItem";
 import ProductCard from "../Cards/ProductCard";
-import ImageList from "./ImageListContainer";
 import { makeStyles } from "@material-ui/core/styles";
 import OfferCard from "./../Cards/OfferCard";
+import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import "../../styles/App.css";
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -16,7 +22,7 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: "space-between",
   },
   list: {
-    maxWidth: "calc(100% - 200px)",
+    maxWidth: "calc(100% - 18vw)",
     padding: "0px 20px",
     backgroundColor: "white",
     borderRadius: "4px",
@@ -35,17 +41,39 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: "space-between",
     padding: "0px 15px",
   },
+  root: {
+    overflow: "hidden",
+    position: "relative",
+    display: "flex",
+  },
+  imageList: {
+    flexWrap: "nowrap",
+    transform: "translateZ(0)",
+    overflow: "scroll",
+    scrollBehavior: "smooth",
+  },
+  imageListExpanded: {
+    flexWrap: "wrap",
+  },
 }));
 
 const CardContainer = ({ title, anchor, id, start, end }) => {
-  const selectedList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
   const classNames = useStyles();
+  const { productReducer } = useSelector(getProducts);
   const [openCardContainer, setOpenCardConatiner] = React.useState(false);
   const [hasOffer, setHasOffer] = React.useState(false);
+  const [products, setProducts] = React.useState([]);
+  const [leftVar, setLeftVar] = React.useState(0);
+  const [maxLeft, setMaxLeft] = React.useState(undefined);
 
   React.useEffect(() => {
     if (anchor) setHasOffer(true);
   }, [anchor]);
+
+  React.useEffect(() => {
+    const products = productReducer.slice(start - 1, end);
+    setProducts(products);
+  }, [productReducer, end, start]);
 
   return (
     <Paper
@@ -66,25 +94,56 @@ const CardContainer = ({ title, anchor, id, start, end }) => {
             </IconButton>
           </button>
         </Grid>
-        {openCardContainer ? (
-          <Grid item container spacing={0}>
-            {selectedList.map((item, index) => {
-              return (
-                <Grid item xs={2} key={index}>
-                  <ProductCard
-                    props={{
-                      name: "product name",
-                      price: `${item}`,
-                      rating: { rate: 2.5 },
-                    }}
-                  />
-                </Grid>
+        <div className={classNames.root}>
+          <button
+            className={`z-10 left-0 bg-gray-100 border top-36 px-6 py-6 rounded-full ${
+              leftVar <= 0 || openCardContainer ? "hidden" : "absolute"
+            }`}
+            onClick={() => {
+              document.getElementById(id).scrollLeft -= 360;
+              setLeftVar((state) => state - 360);
+            }}
+          >
+            <ArrowBackIcon fontSize="small" />
+          </button>
+
+          <ImageList
+            className={
+              (openCardContainer
+                ? classNames.imageListExpanded
+                : classNames.imageList) + " imageList"
+            }
+            cols={"auto"}
+            id={id}
+          >
+            {products.map((item, index) => (
+              <ImageListItem
+                key={index}
+                style={{
+                  height: "auto",
+                  margin: "5px 0px",
+                }}
+              >
+                <ProductCard props={item} />
+              </ImageListItem>
+            ))}
+          </ImageList>
+          <button
+            className={`z-10 right-0 bg-gray-100 border top-36 px-6 py-6 rounded-full ${
+              leftVar >= maxLeft || openCardContainer ? "hidden" : "absolute"
+            }`}
+            onClick={() => {
+              document.getElementById(id).scrollLeft += 360;
+              setLeftVar((state) => state + 360);
+              setMaxLeft(
+                document.getElementById(id).scrollWidth -
+                  document.getElementById(id).clientWidth
               );
-            })}
-          </Grid>
-        ) : (
-          <ImageList id={id} start={start} end={end} />
-        )}
+            }}
+          >
+            <ArrowForwardIcon fontSize="small" />
+          </button>
+        </div>
       </Grid>
       {anchor && <OfferCard parent="container" />}
     </Paper>
