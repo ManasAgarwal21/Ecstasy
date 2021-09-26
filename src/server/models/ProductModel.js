413adb15f4609e7cf44cd5b539441e87da85312d
rupin-1801 <rupinbhugra1411@gmail.com>
2021-09-26 21:59:54 +0530

    added mongoose schema for products and hide mongo url from server.js using config.js file and updated gitignore for that

---
+++ src/server/models/ProductModel.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+export const ProductSchema = new Schema({
+    name: {
+        type: String,
+        required: "Name is required"
+    },
+    seller:{
+        type: String,
+        required: "seller is required"
+    },
+    image:{
+        type: String,
+        required: "image is required"
+    },
+    rating: {
+        type: Number,
+        default: 0
+    },
+    price: {
+        type: Number,
+        required: "price cannot be empty"
+    },
+    onSale: {
+        type: Boolean,
+        default: false
+    },
+    tags: [String]
+})
