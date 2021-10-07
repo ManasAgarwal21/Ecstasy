84ab495862c2e8d308bfecd109d7e2f4f80baa98
rupin-1801 <rupinbhugra1411@gmail.com>
2021-10-07 23:03:55 +0530

    added searchedlist.jsx and updated conditional routing of home content and searchedlist in home component

--- src/client/pages/Home.jsx
+++ src/client/pages/Home.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import { Redirect } from "react-router-dom";
+import { Redirect, useLocation } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Categories from "../components/Categories";
 import Sidebar from "../components/Sidebar";
 import HomeContent from "../components/HomeContent";
 import Footer from "../components/Footer";
+import SearchedList from "../components/SearchedList";
 
 export default function Home({ location }) {
   const [isSidebarOpen, setIsSidebarOpen] = React.useState(false);
+  const { search } = useLocation();
+  const match = search.match(/search=(.*)/);
+  const type = match?.[1];
 
   if (!location.state) {
     return <Redirect to="/login" />;
@@ -21,7 +25,8 @@ export default function Home({ location }) {
         setIsSidebarOpen={setIsSidebarOpen}
       />
       <Categories />
-      <HomeContent />
+      {!type && <HomeContent/>}
+      {type === "search" && <SearchedList/>}
       <Footer />
     </React.Fragment>
   );
