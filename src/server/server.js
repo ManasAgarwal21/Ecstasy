8532d69fd68e53b6f7433b1bc7027d12e4ca933e
rupin-1801 <rupinbhugra1411@gmail.com>
2021-10-24 19:26:01 +0530

    added serving static pages form server.js file

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import routes from "./routes/routes";
 import config from "./config/config.js";
+import path from "path";
+import { getSearchedProducts } from "./controller/product.controller";
 
 const app = express();
 
@@ -9,7 +11,7 @@ mongoose.connect(config.mongodb_uri);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.resolve(__dirname, "./../../build")));
 app.use("/", routes);
 
 mongoose.connection.on("connected", () => {
@@ -20,6 +22,10 @@ mongoose.connection.on("error", () => {
   console.log("error");
 });
 
+
 app.listen(config.port, () => {
   console.log("success");
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './../../build', 'index.html'));
+  });
 });
